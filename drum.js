29ddd4e932093f36817a13d39c8c8d1b79dde53f
Vera-poms <82219325+Vera-poms@users.noreply.github.com>
2023-08-31 14:10:40 +0000

    moved the js from the html to a seaparate js file

--- drum.js
+++ drum.js
@@ -1,3 +1,17 @@
-const text = '["Ford", "BMW", "Kia"]';
-const myArr = JSON.parse(text);
-console.log(myArr[0]);
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
